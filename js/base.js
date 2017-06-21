e2d38a478529b5011dc07163d0fdc150e4aeead0
at-yenho <hothiyen30@gmail.com>
2017-06-21 15:54:14 +0700

    ver0

---
+++ js/base.js
@@ -0,0 +1,11 @@
+<script src="js/jquery-latest.js">
+     
+    //path of use JQuery 
+</script>
+
+<script>
+        function myFunction() {            
+            document.getElementById("myDropdown").classList.toggle("show");     
+            
+        }
+    }
